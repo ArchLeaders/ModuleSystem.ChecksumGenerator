dd0e8e45146113d2f4007dff4e9f7f6d9f3ed61b
ArchLeaders <archleadership28@gmail.com>
2023-12-07 09:41:37 -0800

    Implement Generator & main

---
+++ src/Generator.cs
@@ -0,0 +1,70 @@
+﻿using ModuleSystem.ChecksumGenerator.Extensions;
+using SarcLibrary;
+using Standart.Hash.xxHash;
+using System.Buffers.Binary;
+
+namespace ModuleSystem.ChecksumGenerator;
+
+public class Generator
+{
+    private readonly List<ulong> _keys = [];
+    private readonly List<ulong> _checksums = [];
+
+    public void Collect(string inputFolder, int version)
+    {
+        ZstdExtension zstd = new(Path.Combine(inputFolder, "Pack", "ZsDic.pack.zs"));
+        foreach (var file in Directory.GetFiles(inputFolder, "*.*", SearchOption.AllDirectories)) {
+            CollectFile(file, File.ReadAllBytes(file), inputFolder, version, zstd);
+        }
+    }
+
+    public void Write(in Stream output)
+    {
+        Span<byte> qword = stackalloc byte[sizeof(ulong)];
+
+        foreach (var key in _keys) {
+            BinaryPrimitives.WriteUInt64LittleEndian(qword, key);
+            output.Write(qword);
+        }
+
+        foreach (var checksum in _checksums) {
+            BinaryPrimitives.WriteUInt64LittleEndian(qword, checksum);
+            output.Write(qword);
+        }
+    }
+
+    public void CollectFile(string file, byte[] src, string root, int version, ZstdExtension zstd)
+    {
+        byte[] buffer = zstd.Decompress(src, file);
+        AddChecksum(Path.Combine(file), buffer, root, version);
+
+        if (buffer.Length <= 0) {
+            return;
+        }
+
+        if (buffer.Length > 4 && buffer.AsSpan()[0..4].SequenceEqual("SARC"u8)) {
+            SarcFile sarc = SarcFile.FromBinary(buffer);
+            foreach ((var fileName, var data) in sarc) {
+                CollectFile(Path.Combine(file, fileName), data, root, version, zstd);
+            }
+        }
+    }
+
+    public void AddChecksum(string file, byte[] buffer, string root, int version)
+    {
+        string path = Path.GetRelativePath(root, file).Replace('\\', '/');
+        ulong key = xxHash64.ComputeHash(path);
+        ulong checksum = xxHash64.ComputeHash(buffer);
+
+        int index;
+
+        // If the key exists with a different
+        // value add a version specific entry
+        if ((index = _keys.IndexOf(key)) >= 0 && _checksums[index] != checksum) {
+            key = xxHash64.ComputeHash(path += $"#{version}");
+        }
+
+        _keys.Add(key);
+        _checksums.Add(checksum);
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ModuleSystem.ChecksumGenerator;
+using ModuleSystem.ChecksumGenerator.Extensions;
+using System.Reflection;
+
+Console.WriteLine($"""
+    Module System Checksum Generator [Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "Undefined"}]
+    (c) Arch Leaders. MIT License
+
+    """);
+
+if (args.Length <= 0 || args[0] is "-h" or "--help") {
+    Console.WriteLine("""
+        Usage:
+          ChecksumGenerator <path1|path2> [-o|--output OUTPUT]
+        """);
+
+    return;
+}
+
+List<string> inputFolders = args[0].Split('|')
+    .Where(x => File.Exists(Path.Combine(x, "Pack", "ZsDic.pack.zs")))
+    .ToList();
+
+if (inputFolders.Count <= 0) {
+    throw new ArgumentException("No valid input folder!");
+}
+
+
+Flags flags = Flags.Parse(args[2..]);
+
+string outputFile = flags.Get("ChecksumMap.bin", "o", "output");
+Console.WriteLine($"Registered output path: '{outputFile}'");
+
+Generator generator = new();
+foreach (var inputFolder in inputFolders) {
+    Console.WriteLine($"Processing: {inputFolder}");
+    generator.Collect(inputFolder, inputFolder.TryGetVersion());
+}
+
+Directory.CreateDirectory(Path.GetDirectoryName(outputFile) ?? string.Empty);
+using FileStream fs = File.Create(outputFile);
+generator.Write(fs);
